6bb0677b2d35870267209df316b6293c0c7c857e
Aryansh <aryanshappmaker@gmail.com>
2021-05-06 07:42:53 +0530

    Initial Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# Number Systems
+[![forthebadge made-with-python](http://ForTheBadge.com/images/badges/made-with-python.svg)](https://www.python.org/)
+[![forthebadge](https://forthebadge.com/images/badges/built-with-love.svg)](https://instagram.com/_aryansh.gupta_)
+
+**Python Module To Quickly Convert One Number System to Another.**
+
+## Installations
+Insall using ```pip```
+
+```shell
+pip install numbersystems
+```
+
---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name = 'numbersystems',
+    version='1.0.1',
+    author = 'Aryansh Gupta',
+    author_email = 'Aryanshappmaker@gmail.com',
+    description = 'Python Module to allow the use of Vectors',
+    py_modules = ["numbersystems"],
+    package_dir = {'':'src'},
+    long_description = long_description+'<br>Full Documentaion can be found on [ReadTheDocs](https:///VectorsPY.readthedocs.io)',
+    long_description_content_type = 'text/markdown',
+    url = 'https://github.com/aryansh-1/numbersystems',
+    python_requires = '>=3.1',
+)
